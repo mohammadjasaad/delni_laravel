23a23fce8400c4a04e77d6d0d2f1adddbb0c700a
Mohammad Asaad <youremail@example.com>
2025-11-19 15:14:43 +0000

    update

--- storage/framework/views/b8a223ecac36a193cec404ce3e62ce46.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', 'Forbidden'); ?>
-<?php $__env->startSection('code', '403'); ?>
-<?php $__env->startSection('message', __('messages.error_403_message', [], app()->getLocale()) ?? 'Access denied'); ?>
-
-<?php echo $__env->make('errors.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/delni_user/delni/resources/views/errors/403.blade.php ENDPATH**/ ?>
